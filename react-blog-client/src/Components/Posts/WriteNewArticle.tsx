9646f18c599ee516bbf8aeef33b39cf26cef1538
ELFrancisco4 <offiongfrancis14@gmail.com>
2023-11-07 16:15:02 +0100

    <fix: Resolve type error in 'WriteNewArticle.tsx'>

--- react-blog-client/src/Components/Posts/WriteNewArticle.tsx
+++ react-blog-client/src/Components/Posts/WriteNewArticle.tsx
@@ -19,14 +19,16 @@ const WriteNewArticle: React.FC = () => {
   return (
     <div className="write-new-article">
       <nav>
-        <span>
-          <img src={logo} alt="" /> <strong>QuillQuest</strong>
-        </span>
-        <span>Drafts in 0xKali</span>
+        <div>
+          <span>
+            <img src={logo} alt="" /> <strong>QuillQuest</strong>
+          </span>
+          <span>Drafts in 0xKali</span>
+        </div>
 
         <ul>
           <li>
-            <span>Publish</span>
+            <span className="publish-article">Publish</span>
           </li>
           <li>
             <AiOutlineBell />
@@ -37,39 +39,39 @@ const WriteNewArticle: React.FC = () => {
         </ul>
       </nav>
 
-      <div className="toolbar">
-        <button className="icon-button close">
-          <IoMdClose />
-        </button>
-        <div className="icons">
-          <button className="icon-button">
-            <FaCamera />
+      <div className="article-content">
+        <div className="toolbar">
+          <button className="icon-button close">
+            <IoMdClose />
           </button>
-          <button className="icon-button">
-            <FaVideo />
-          </button>
-          <button className="icon-button">
-            <FaMicrophone />
-          </button>
-          <button className="icon-button">
-            <FaLink />
-          </button>
-          <button className="icon-button">
-            <FaCode />
-          </button>
-          <button className="icon-button">
-            <FaEllipsisH />
+          <div className="icons">
+            <button className="icon-button">
+              <FaCamera />
+            </button>
+            <button className="icon-button">
+              <FaVideo />
+            </button>
+            <button className="icon-button">
+              <FaMicrophone />
+            </button>
+            <button className="icon-button">
+              <FaLink />
+            </button>
+            <button className="icon-button">
+              <FaCode />
+            </button>
+            <button className="icon-button">
+              <FaEllipsisH />
+            </button>
+          </div>
+          <button className="icon-button check">
+            <FaCheckCircle />
           </button>
         </div>
-        <button className="icon-button check">
-          <FaCheckCircle />
-        </button>
-      </div>
-
-      <div>
-        <input type="text" placeholder="Title" />
-
-        <textarea name="name" id="text-area" cols="30" rows="10"></textarea>
+        <div className="article-content-body">
+          <input type="text" placeholder="Title" />
+          <textarea name="name" id="text-area" cols={30} rows={10}></textarea>
+        </div>
       </div>
     </div>
   );
